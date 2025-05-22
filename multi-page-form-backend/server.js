2c0cffa311af9fb99ed35e6efe01485c71d7445f
Vikram <vikramranjan71122@gmail.com>
2025-05-22 11:01:25 +0530

    env setup

--- multi-page-form-backend/server.js
+++ multi-page-form-backend/server.js
@@ -10,18 +10,7 @@ const prisma = new PrismaClient();
 
 const port = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://multi-page-form-92c89huqc-vilram-ranjans-projects.vercel.app",
-];
-
-app.use(
-  cors({
-    origin: allowedOrigins,
-  })
-);
-
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 
 // ✅ Add a root path route
